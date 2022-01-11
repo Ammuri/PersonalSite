12794cdc2dd9030b519ccc593a00eea81b23fad9
Ammuri <springabdelati@gmail.com>
2022-01-10 19:39:28 -0500

    Initial Commit

---
+++ src/test/java/PersonalProjects/PersonalSite/PersonalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package PersonalProjects.PersonalSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
